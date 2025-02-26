1668d795380da734d47fbccaeaeca06313b2ee95
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-02-25 21:41:17 -0300

    JavaScript Aula3
    Primeiros exercícios em JavaScript

---
+++ programacaoEmScripts/aula03/aula3ex01/script.js
@@ -0,0 +1,15 @@
+/* alert("Olá")
+console.log("Mensagem direta no console");
+nome = 'Maria';
+var Nome = "Carlos";
+let nomE = "Roberto";
+const PI = 3.1415; */
+
+let nome = prompt("Informe seu nome:");
+let sobrenome = prompt("Informe seu sobrenome:")
+alert("Seu nome completo: "+nome+' '+sobrenome);
+
+let num1 = parseFloat(prompt("Informe um numero:"));
+let num2 = parseFloat(prompt("Informe outro numero:"));
+let soma = num1 + num2;
+alert("A soma dos valores é "+ soma)
