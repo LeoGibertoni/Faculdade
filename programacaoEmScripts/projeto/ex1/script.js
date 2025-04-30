dd4e73b65a3fcba7e0fd10c574666f184baadec3
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-04-29 21:53:32 -0300

    Projeto Script

---
+++ programacaoEmScripts/projeto/ex1/script.js
@@ -0,0 +1,8 @@
+class livro{
+    constructor(codigo, titulo, editora, ano){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.editora = editora;
+        this.ano = ano;
+    }
+}
