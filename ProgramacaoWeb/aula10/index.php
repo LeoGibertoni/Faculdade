9bef848ca7ba684b888d96b3b64ec37439458a75
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-10-16 21:37:57 -0300

    Projeto PHP com MySql

---
+++ ProgramacaoWeb/aula10/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto PHP com Banco</title>
+    <style>
+        a{
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <h2>Sistema de cadastro de Pessoas</h2>
+    <br><br>
+    <button><a href="pagina1.php">Inserir</a></button>
+    <button><a href="pagina2.php">Excluir</a></button>
+    <button><a href="pagina3.php">Atualizar</a></button>
+    <button><a href="consultar.php">Consultar</a></button>
+</body>
+</html>
---
+++ ProgramacaoWeb/aula10/pagina2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto PHP com Banco</title>
+</head>
+<body>
+    <h3>Excluir cadastro pessoa</h3>
+    <form method="POST" action="excluir.php">
+        <label for="codigo">Código</label>
+        <input type="number" name="codigo"><br><br>
+        <button type="submit">Excluir</button>
+    </form>
+    <br>
+    <button><a href="index.php" style="color: black">Voltar</a></button>
+</body>
+</html>
